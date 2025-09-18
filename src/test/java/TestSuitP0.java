43e8f2e88186bbbfcb55af4387c6df905b5b59c9
Adeel <adeelkhan775@gmail.com>
2025-09-18 17:20:09 +0500

    First push

---
+++ src/test/java/TestSuitP0.java
@@ -0,0 +1,30 @@
+import org.testng.annotations.Test;
+
+public class TestSuitP0 {
+	
+	@Test
+	public static void TC1() {
+	
+		System.out.println("TC 1 Executed");
+		
+	}
+
+	@Test
+	public static void TC2() {
+	
+		System.out.println("TC 2 Executed");
+		
+	}
+	@Test
+	public static void TC3() {
+	
+		System.out.println("TC 3 Executed");
+		
+	}
+	@Test
+	public static void TC4() {
+	
+		System.out.println("TC 4 Executed");
+		
+	}
+}
